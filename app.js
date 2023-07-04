a8c9fe0d6a0a11bb25d84e16c8e26b5281e5525e
hiitgmanshu <himanshushekhar44578@gmail.com>
2023-07-04 18:41:48 +0530

    process env port added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
